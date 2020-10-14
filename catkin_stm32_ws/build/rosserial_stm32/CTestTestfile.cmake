faac8d855ad6a53a1a9c2c2c0b198ccbd140e73e
CaesarLinxw <xiaowei.lin1997@gmail.com>
2020-10-14 16:16:36 +0800

    ROS_Serial_test updated, message transmission done, sync problem needed to be fixed

---
+++ catkin_stm32_ws/build/rosserial_stm32/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/caesar/Desktop/ROS/catkin_stm32_ws/src/rosserial_stm32
+# Build directory: /home/caesar/Desktop/ROS/catkin_stm32_ws/build/rosserial_stm32
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
