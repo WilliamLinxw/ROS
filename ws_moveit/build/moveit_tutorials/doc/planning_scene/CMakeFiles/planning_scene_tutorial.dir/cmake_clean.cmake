d94a64c113294910db79146a0bde5b9874190157
CaesarLinxw <xiaowei.lin1997@gmail.com>
2020-12-03 19:30:07 +0800

    workspace for moveit added

---
+++ ws_moveit/build/moveit_tutorials/doc/planning_scene/CMakeFiles/planning_scene_tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/planning_scene_tutorial.dir/src/planning_scene_tutorial.cpp.o"
+  "/home/caesar/Desktop/ROS/ws_moveit/devel/.private/moveit_tutorials/lib/moveit_tutorials/planning_scene_tutorial.pdb"
+  "/home/caesar/Desktop/ROS/ws_moveit/devel/.private/moveit_tutorials/lib/moveit_tutorials/planning_scene_tutorial"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/planning_scene_tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ws_moveit/build/moveit_tutorials/doc/visualizing_collisions/CMakeFiles/visualizing_collisions_tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/visualizing_collisions_tutorial.dir/src/visualizing_collisions_tutorial.cpp.o"
+  "/home/caesar/Desktop/ROS/ws_moveit/devel/.private/moveit_tutorials/lib/moveit_tutorials/visualizing_collisions_tutorial.pdb"
+  "/home/caesar/Desktop/ROS/ws_moveit/devel/.private/moveit_tutorials/lib/moveit_tutorials/visualizing_collisions_tutorial"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/visualizing_collisions_tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
