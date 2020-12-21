7a0681745029cec2921062daaaa611ff75b597db
CaesarLinxw <xiaowei.lin1997@gmail.com>
2020-12-21 18:37:53 +0800

    moveit config tested

---
+++ ws_moveit/build/arm_assembly_3_moveit_config/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/caesar/Desktop/ROS/ws_moveit/src/arm_assembly_3_moveit_config
+# Build directory: /home/caesar/Desktop/ROS/ws_moveit/build/arm_assembly_3_moveit_config
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
--- ws_moveit/build/moveit_tutorials/catkin_generated/setup_cached.sh
+++ ws_moveit/build/moveit_tutorials/catkin_generated/setup_cached.sh
@@ -8,6 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/caesar/Desktop/ROS/ws_moveit/devel/.private/moveit_tutorials:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/caesar/Desktop/ROS/ws_moveit/devel/.private/moveit_tutorials/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/caesar/Desktop/ROS/ws_moveit/devel/.private/moveit_tutorials/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/caesar/Desktop/ROS/ws_moveit/build/moveit_tutorials'
 export ROSLISP_PACKAGE_DIRECTORIES="/home/caesar/Desktop/ROS/ws_moveit/devel/.private/moveit_tutorials/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/caesar/Desktop/ROS/ws_moveit/src/moveit_tutorials:$ROS_PACKAGE_PATH"
