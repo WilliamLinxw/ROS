f2ab1a2857fc970553cb79cb142841b1c49a0cc2
CaesarLinxw <xiaowei.lin1997@gmail.com>
2020-12-25 14:58:41 +0800

    arm_assembly_6_moveit_config added, working

---
+++ ws_moveit/build/arm_assembly_6_moveit_config_1/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googlemock
+# Build directory: /home/caesar/Desktop/ROS/ws_moveit/build/arm_assembly_6_moveit_config_1/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
