2ab79d8c0bfbdbba9ea1323ca94e58bb40115116
CaesarLinxw <xiaowei.lin1997@gmail.com>
2021-01-14 13:46:45 +0800

    script for moving a circle added

--- xarm_ws/src/xarm_planner/scripts/xarm_circle.py
+++ xarm_ws/src/xarm_planner/scripts/xarm_circle.py
@@ -1,12 +1,14 @@
+#!/usr/bin/env python
+
 import sys
 import copy
 import rospy
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
-from moveit_python import *
-from math import pi
-from std_msgs import String
+from math import *
+from std_msgs.msg import String
+import numpy as np
 
 class MoveCircle(object):
     def __init__(self):
@@ -21,20 +23,21 @@ class MoveCircle(object):
         scene = moveit_commander.PlanningSceneInterface()
 
         group_name = 'xarm7'
-        group = moveit_commander.MoveGroupCommander(group_name)
+        move_group = moveit_commander.MoveGroupCommander(group_name)
 
         display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory, queue_size=20)
 
-        print "================ Waiting for Rviz =================="
-        rospy.sleep(10)
-        print "================ Start moving! ================="
+        # We can get the name of the reference frame for this robot:
+        planning_frame = move_group.get_planning_frame()
+        print "============ Planning frame: %s" % planning_frame
 
-        print "============ Reference frame: %s" % group.get_planning_frame()
+        # We can also print the name of the end-effector link for this group:
+        eef_link = move_group.get_end_effector_link()
+        print "============ End effector link: %s" % eef_link
 
-        print "============ End effector link: %s" % group.get_end_effector_link()
-
-        print "============ Robot Groups:"
-        print robot.get_group_names()
+        # We can get a list of all the groups in the robot:
+        group_names = robot.get_group_names()
+        print "============ Available Planning Groups:", robot.get_group_names()
 
         print "============ Printing robot state"
         print robot.get_current_state()
@@ -54,8 +57,9 @@ class MoveCircle(object):
         xarm7 = self.move_group
 
         # Go back to the home position
-        xarm7.go('home_pose', wait=True)
-        xarm7.stop()
+        xarm7.set_named_target('home_pose')
+        xarm7.go(wait=True)
+        rospy.sleep(1)
 
         # Go to a desired position with the center of the circle is
         pose_goal = geometry_msgs.msg.Pose()
@@ -75,26 +79,74 @@ class MoveCircle(object):
         xarm7.clear_pose_targets()
 
         # Get the center of the circle
-        current_pose = xarm.get_current_pose().pose
+        current_pose = xarm7.get_current_pose().pose
         centerX = current_pose.position.x
         centerY = current_pose.position.y
         radius = 0.05
 
         # Create a circle waypoints
         waypoints = []
-        theta = 0
-        while theta <= 4 * pi:
-            target_pose = xarm7.get_current_pose().pose
-            target_pose.position.x = centerX + radius * cos(theta)
-            target_pose.position.y = centerY + radius * sin(theta)
+        # theta = 0
+        # while theta <= 2 * pi:
+        #     target_pose = xarm7.get_current_pose().pose
+        #     target_pose.position.x = centerX + radius * cos(theta)
+        #     target_pose.position.y = centerY + radius * sin(theta)
+        #     waypoints.append(copy.deepcopy(target_pose))
+        #     theta += 0.01
+        #     print(theta)
+        target_pose = xarm7.get_current_pose().pose
+        theta = np.arange(0, 2*np.pi, 0.01)
+        x = centerX + radius * np.cos(theta)
+        y = centerY + radius * np.sin(theta)
+        for i in range(len(x)):
+            target_pose.position.x = x[i]
+            target_pose.position.y = y[i]
             waypoints.append(copy.deepcopy(target_pose))
-            theta += 0.01
+        print("Waypoints: ", waypoints)
 
         # Compute the Cartesian path
         fraction = 0
         maxtrails = 100
-        attepts = 0
+        attempts = 0
+        jump_threshold = 0.0
+        eef_step = 0.01
 
-        while fraction < 1 and attepts < maxtrails:
+        while fraction < 1 and attempts < maxtrails:
+            plan, fraction = xarm7.compute_cartesian_path(waypoints, eef_step, jump_threshold)
+            attempts += 1
+
+            if attempts % 10 == 0:
+                print("Still trying after %d attempts ...", attempts)
+
+        # Execute the plan if it is successfully planned
+        if fraction == 1:
+            print("Path computed successfully. Moving the arm.")
+            xarm7.execute(plan, wait=True)
+        else:
+            print("Path planning failed with only %0.6f success after %d attempts.", fraction, maxtrails)
+        
+        rospy.sleep(1)
+
+        print "============ Motion finished ============"
+
+def main():
+    arm = MoveCircle()
+
+    print "============ Press `Enter` to execute a movement function ..."
+    raw_input()
+    arm.move_circle()
+
+if __name__ == '__main__':
+    while not rospy.is_shutdown():
+        try:
+            main()
+            break
+        except rospy.ROSInterruptException:
+            break
+        except KeyboardInterrupt:
+            break
+
+    
+
         
 
